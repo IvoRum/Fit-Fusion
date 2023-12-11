e4035961ea827cc0dd8ddf9166ec0d48f171efb7
IvoRum <ivailo.rumenov@outlook.com>
2023-12-11 09:09:54 +0200

    Created two new queries and managaed to return a fuild.

--- Fit-Fusion/Fit-Fusion/Controllers/ClientController.cs
+++ Fit-Fusion/Fit-Fusion/Controllers/ClientController.cs
@@ -17,8 +17,7 @@ namespace Fit_Fusion.Controllers
         public ActionResult<string> Index()
         {
             ClientRepo clientRepo = new ClientRepo();
-            clientRepo.getClientInfo();
-            return "alo da";
+            return clientRepo.getClientInfo();
         }
     }
 }
--- Fit-Fusion/Fit-Fusion/Data/ClientRepo.cs
+++ Fit-Fusion/Fit-Fusion/Data/ClientRepo.cs
@@ -5,8 +5,8 @@ namespace Fit_Fusion.Data;
 public class ClientRepo
 {
     string connectionString = "Server=ep-lively-queen-99751851.eu-central-1.aws.neon.tech; Database =Fit_Fusion; Username =IvoRum; Password=RXvnYfB9m1Uz;";
-
-    public void getClientInfo()
+    
+    public string getClientInfo()
     {
         using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
         {
@@ -14,6 +14,34 @@ public class ClientRepo
     
             string query = "SELECT * FROM Client";
             
+            using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Access data using reader
+                        return reader["first_name"].ToString();
+                    }
+                }
+            }
+    
+            connection.Close();
+        }
+
+        return null;
+    }
+
+    public void getWorkerStats()
+    {
+        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            string query =
+                "select worker.first_name,worker.last_name,r.title,s.amount,worker.first_work_day_date from worker " +
+                "join public.role r on r.id = worker.role " +
+                "join public.salary s on worker.id = s.worker " +
+                "order by worker.first_work_day_date;";
             using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
             {
                 using (NpgsqlDataReader reader = command.ExecuteReader())
@@ -29,4 +57,30 @@ public class ClientRepo
             connection.Close();
         }
     }
+    
+    public void getBalance()
+    {
+        using (NpgsqlConnection connection = new NpgsqlConnection(connectionString))
+        {
+            connection.Open();
+            string query =
+                "select balance.date,balance.net_income,bt.name from balance " +
+                "join public.balance_type bt on bt.id = balance.type " +
+                "order by balance.date;";
+            using (NpgsqlCommand command = new NpgsqlCommand(query, connection))
+            {
+                using (NpgsqlDataReader reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        // Access data using reader
+                        Console.WriteLine(reader["first_name"]);
+                    }
+                }
+            }
+    
+            connection.Close();
+        }
+    }
+
 }
